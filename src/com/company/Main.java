bf736b17c2f2bd61898861d3c284620bcccbb153
emmaofthevoid <tehurapardo@outlook.es>
2022-05-17 15:43:39 +0200

    alles

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import com.company.Uebung1.Article;
+import com.company.Uebung1.Order;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Article schocko = new Article("Schocko", 5, 10.99);
+        Article kekse = new Article("Kekse", 2, 4.99);
+        Article sojamilch = new Article("Sojamilch", 3, 3.99);
+        Article teuereSache = new Article("Ich bin eine teuere Sache", 6, 130.99);
+        Order order = new Order(4);
+
+        order.addArticle(schocko);
+        order.addArticle(kekse);
+        order.addArticle(sojamilch);
+        order.addArticle(teuereSache);
+
+        System.out.println(order);
+        System.out.println(order.findMostExpensiveArticle());
+        System.out.println(order.findMostExpensiveOrderPosition());
+        System.out.println(order.sumOrder());
+
+        System.out.println("*****");
+        System.out.println(order.calculateTax());
     }
 }
--- src/com/company/Uebung1/Order.java
+++ src/com/company/Uebung1/Order.java
@@ -7,10 +7,10 @@ public class Order {
     private int capacityOrder;
     private Article[] articles;
 
-    public Order(int nrArticles, int capacityOrder, Article[] articles) {
+    public Order(int capacityOrder) {
         this.nrArticles = 0;
         this.capacityOrder = capacityOrder;
-        articles = new Article[capacityOrder];
+        this.articles = new Article[capacityOrder];
     }
 
     public int getNrArticles() {
@@ -25,18 +25,70 @@ public class Order {
     public String toString() {
         for (Article a : articles) {
 
-
             return "The Order contains these articles: " +
                     Arrays.toString(articles);
         }
         return null;
     }
 
-    public void addArticle (Article a) {
-        articles[nrArticles] = a;
+    public void addArticle(Article a) {
+        articles[nrArticles++] = a;
     }
 
-    public void findMostExpensiveArticle
+    public int findMostExpensiveArticle() {
 
+        int mostExpensiveItem = 0;
+        double highestPrice = 0;
+
+        for (int i = 0; i < articles.length; i++) {
+            if (articles[i].getPrice() > highestPrice) {
+                highestPrice = articles[i].getPrice();
+                mostExpensiveItem = i;
+            }
+        }
+        return mostExpensiveItem + 1;
+    }
+
+    public int findMostExpensiveOrderPosition() {
+        int index = 0;
+        double mult = 0;
+
+        for (int i = 0; i < articles.length; i++) {
+            if (articles[i].getPrice() * articles[i].getNumber() > mult) {
+                mult = articles[i].getPrice() * articles[i].getNumber();
+                index = i;
+            }
+        }
+        return index + 1;
+    }
+
+    public double sumOrder() {
+        double summe = 0;
+        for (Article a : articles) {
+            summe += a.getPrice();
+        }
+        return summe;
+    }
+
+    public double calculateTax() {
+        double sum = 0;
+        for (Article a : articles) {
+            if (a.getPrice() < 20) {
+                sum += 5 * a.getPrice() / 100;
+            }
+            if (a.getPrice() > 20) {
+                sum += 20 * a.getPrice() / 100;
+            }
+        }
+        return sum;
+    }
+
+    public void sumOrderWithTax() {
+        int sum = 0;
+        for (Article a : articles) {
+
+        }
+
+    }
 
 }
